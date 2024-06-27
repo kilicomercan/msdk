dfcb02ed81f72565e2ccf7fa79f6c7bfe18173c8
ANALOG\OKilic <Omercan.Kilic@analog.com>
2024-06-27 12:30:36 +0300

    Reserve eng from elf completed. Build is succesfull for test project

--- Libraries/MiscDrivers/ACCEL/ADXL/adxl363.c
+++ Libraries/MiscDrivers/ACCEL/ADXL/adxl363.c
@@ -1,338 +1,300 @@
-
 #include "adxl363.h"
 
-int __adxl363_reg_read(uint8_t reg_addr, uint8_t *read_buff,uint size)
+#define SAMPLE_SIZE_IN_BYTES 2
+#define MAX_FIFO_SAMPLE_COUNT 511
+#define ADXL_363_FIFO_TX_BUFF_SIZE (MAX_FIFO_SAMPLE_COUNT * SAMPLE_SIZE_IN_BYTES + 1)
+#define ADXL_363_FIFO_RX_BUFF_SIZE (MAX_FIFO_SAMPLE_COUNT * SAMPLE_SIZE_IN_BYTES + 1)
+
+static uint8_t read_reg_pack[3];
+static uint8_t write_reg_pack[3];
+static uint8_t adxl_363_fifo_tx_buff[ADXL_363_FIFO_TX_BUFF_SIZE];
+static uint8_t adxl_363_fifo_rx_buff[ADXL_363_FIFO_RX_BUFF_SIZE];
+
+static mxc_spi_req_t read_reg_req;
+static mxc_spi_req_t write_reg_req;
+static mxc_spi_req_t read_fifo_req;
+
+int __adxl363_reg_read(uint8_t reg_addr, uint8_t *read_buff, uint32_t size)
 {
-  int iVar1;
-  
-  read_reg_pack[0] = '\v';
-  read_reg_req.txData = read_reg_pack;
-  read_reg_req.txLen = size + 2;
-  read_reg_req.ssDeassert = 1;
-  read_reg_pack[1] = reg_addr;
-  read_reg_req.rxData = read_buff;
-  read_reg_req.rxLen = read_reg_req.txLen;
-  iVar1 = MXC_SPI_MasterTransaction(&read_reg_req);
-  return iVar1;
+    int ret_val = 0;
+    read_reg_pack[0] = ADXL363_CMD_READ;
+    read_reg_pack[1] = reg_addr;
+
+    read_reg_req.txData = read_reg_pack;
+    read_reg_req.txLen = size + 2;
+    read_reg_req.ssDeassert = 1;
+    read_reg_req.rxData = read_buff;
+    read_reg_req.rxLen = read_reg_req.txLen;
+    ret_val = MXC_SPI_MasterTransaction(&read_reg_req);
+    return ret_val;
 }
 
-int __initialize_mxc_spi(adxl_363_spi_t spi_conf,adxl_363_controller_t adxl_conf)
+int __initialize_mxc_spi(adxl_363_spi_t spi_conf, adxl_363_controller_t adxl_conf)
 {
-  mxc_spi_pins_t pins;
-  int iVar1;
-  uint in_r1;
-  undefined4 in_r2;
-  undefined4 in_r3;
-  mxc_spi_regs_t *spi;
-  
-  if (((uint8_t)adxl_conf & 0xff) == 0) {
-    spi = (mxc_spi_regs_t *)0x40046000;
-  }
-  else if (((uint8_t)adxl_conf & 0xff) == 1) {
-    spi = (mxc_spi_regs_t *)0x400be000;
-  }
-  else {
-    spi = (mxc_spi_regs_t *)0x0;
-  }
-  pins._4_4_ = in_r3;
-  pins._0_4_ = in_r2;
-  pins.vddioh = (_Bool)spi_conf.spi_id;
-  iVar1 = MXC_SPI_Init(spi,1,0,1,0,in_r1,pins);
-  return iVar1;
+    mxc_spi_regs_t *spi;
+    spi = MXC_SPI_GET_SPI(spi_conf.spi_id);
+
+    /**
+     * @warning MXC_SPI_Init is defined differently for different part numbers.
+     * We call the implementation of it for MAX32655. 
+     * 
+     * @todo Update this part of the function to make it compatible for other
+     * part numbers.
+    */
+    return MXC_SPI_Init(spi, 1, 0, 1, 0, spi_conf.spi_speed, spi_conf.spi_pins);
 }
 
-int16_t __adxl363_read_axis_data(uint8_t lsig, uint8_t msig, int *err)
+int16_t __adxl363_read_axis_data(uint8_t lsig_addr, uint8_t msig_addr, int *err)
 {
-  int iVar1;
-  uint8_t most_sig;
-  uint8_t least_sig;
-  
-  least_sig = '\0';
-  most_sig = '\0';
-  iVar1 = adxl363_reg_read(lsig, &least_sig);
-  *err = iVar1;
-  if (iVar1 == 0) {
-    iVar1 = adxl363_reg_read(msig, &most_sig);
-    *err = iVar1;
-  }
-  return CONCAT11(most_sig,least_sig);
+    int ret_val = 0;
+    uint8_t most_sig = 0;
+    uint8_t least_sig = 0;
+
+    ret_val = adxl363_reg_read(lsig_addr, &least_sig);
+    *err = ret_val;
+    if (ret_val == 0) {
+        ret_val = adxl363_reg_read(msig_addr, &most_sig);
+        *err = ret_val;
+    }
+    /**
+     * @todo check concatenation.
+    */
+    return (most_sig << 8 | least_sig);
 }
+
+void __reset_variables(void)
+{
+    memset(&read_reg_req,0, sizeof(mxc_spi_req_t));
+    memset(&write_reg_req,0, sizeof(mxc_spi_req_t));
+    memset(&read_fifo_req,0, sizeof(mxc_spi_req_t));
+
+    write_reg_pack[1] = 0;
+    write_reg_pack[2] = 0;
+    read_reg_pack[1] = 0;
+    memset(adxl_363_fifo_tx_buff + 1, 0, ADXL_363_FIFO_TX_BUFF_SIZE-1);
+    memset(adxl_363_fifo_rx_buff, 0, ADXL_363_FIFO_RX_BUFF_SIZE);
+    return;
+}
+
 
 int adxl363_axis_read(int16_t *axis_data, adxl_axis_t axis_id)
 {
-  int16_t iVar1;
-  int ret_val;
-  
-  ret_val = 0;
-  if (axis_id == X_AXIS) {
-    iVar1 = __adxl363_read_axis_data('\x0e','\x0f',&ret_val);
-    *axis_data = iVar1;
-  }
-  else if (axis_id == Y_AXIS) {
-    iVar1 = __adxl363_read_axis_data('\x10','\x11',&ret_val);
-    *axis_data = iVar1;
-  }
-  else if (axis_id == Z_AXIS) {
-    iVar1 = __adxl363_read_axis_data('\x12','\x13',&ret_val);
-    *axis_data = iVar1;
-  }
-  else {
-    *axis_data = -1;
-    ret_val = -4;
-  }
-  return ret_val;
+    int16_t local_axis_val = 0;
+    int ret_val = 0;
+
+    if (axis_id == X_AXIS) {
+        local_axis_val = __adxl363_read_axis_data(ADXL_363_REG_XDATA_L, ADXL_363_REG_XDATA_H, &ret_val);
+        *axis_data = local_axis_val;
+    } else if (axis_id == Y_AXIS) {
+        local_axis_val = __adxl363_read_axis_data(ADXL_363_REG_YDATA_L, ADXL_363_REG_YDATA_H, &ret_val);
+        *axis_data = local_axis_val;
+    } else if (axis_id == Z_AXIS) {
+        local_axis_val = __adxl363_read_axis_data(ADXL_363_REG_ZDATA_L, ADXL_363_REG_ZDATA_H, &ret_val);
+        *axis_data = local_axis_val;
+    } else {
+        *axis_data = -1;
+        ret_val = -4;
+    }
+    return ret_val;
 }
 
 int adxl363_enable_measurement(uint8_t enable)
 {
-  int iVar1;
-  uint8_t meas_mode;
-  
-  meas_mode = '\0';
-  iVar1 = adxl363_reg_read('-',&meas_mode);
-  if (iVar1 == 0) {
-    if (enable == '\x01') {
-      meas_mode = meas_mode & 0xfc | 2;
+    int ret_val;
+    uint8_t meas_mode = 0;
+    ret_val = adxl363_reg_read(ADXL_363_REG_POWER_CTL, &meas_mode);
+    if (ret_val == 0) {
+        if (enable == 1) {
+            meas_mode = (meas_mode & 0xFC) | 2;
+        } else {
+            meas_mode = (meas_mode & 0xFC);
+        }
+        ret_val = adxl363_reg_write(ADXL_363_REG_POWER_CTL, meas_mode);
     }
-    else {
-      meas_mode = meas_mode & 0xfc;
-    }
-    iVar1 = adxl363_reg_write('-',meas_mode);
-  }
-  return iVar1;
+    return ret_val;
 }
 
 int adxl363_fifo_enable_mode(uint8_t mode)
 {
-  int iVar1;
-  uint8_t current_status;
-  
-  current_status = '\0';
-  if ((((mode == '\0') || (mode == '\x02')) || (mode == '\x03')) || (mode == '\x01')) {
-    iVar1 = adxl363_reg_read('(',&current_status);
-    if (iVar1 == 0) {
-      current_status = current_status & 0xfc;
-      iVar1 = adxl363_reg_write('(',current_status | mode);
+    int ret_val;
+    uint8_t current_status = 0;
+
+    // If the mode is between [0,3]
+    if ((mode & 0xFC) == 0 ) {
+        ret_val = adxl363_reg_read(ADXL_363_REG_POWER_CTL, &current_status);
+        if (ret_val == 0) {
+            current_status = current_status & 0xfc;
+            ret_val = adxl363_reg_write(ADXL_363_REG_POWER_CTL, current_status | mode);
+        }
+    } else {
+        // If mode is not in valid range, return error.
+        ret_val = -4;
     }
-  }
-  else {
-    iVar1 = -4;
-  }
-  return iVar1;
+    return ret_val;
 }
 
-int adxl363_fifo_get_sample_count(uint8_t *sample_count)
+int adxl363_fifo_get_sample_count(uint16_t *sample_count)
 {
-  int iVar1;
-  uint8_t reg_fifo_sample;
-  uint8_t reg_fifo_cont;
-  
-  if (sample_count != (uint8_t *)0x0) {
-    reg_fifo_cont = '\0';
-    reg_fifo_sample = '\0';
-    iVar1 = adxl363_reg_read('(',&reg_fifo_cont);
-    if (iVar1 == 0) {
-      iVar1 = adxl363_reg_read(')',&reg_fifo_sample);
+    int ret_val = 0;
+    uint8_t reg_fifo_sample = 0;
+    uint8_t reg_fifo_cont = 0;
+
+    if (sample_count != NULL) {
+        ret_val = adxl363_reg_read(ADXL_363_REG_FIFO_CONTROL, &reg_fifo_cont);
+        if (ret_val == 0) {
+            ret_val = adxl363_reg_read(ADXL_363_REG_FIFO_SAMPLES, &reg_fifo_sample);
+        }
+        *sample_count = ((reg_fifo_cont & 8) << 5) | (uint8_t)reg_fifo_sample;
+        return ret_val;
     }
-    *sample_count = (reg_fifo_cont & 8) << 5 | (uint8_t)reg_fifo_sample;
-    return iVar1;
-  }
-  return -3;
+    return ret_val;
 }
 
-int adxl363_fifo_read(uint8_t *buff,uint8_t sample_count)
-
+int adxl363_fifo_read(uint8_t *buff, uint16_t sample_count)
 {
-  int iVar1;
-  size_t __n;
-  
-  if (sample_count < 0x201) {
-    __n = (uint)sample_count * 2;
-    if (buff == (uint8_t *)0x0) {
-      iVar1 = -3;
+    int ret_val = 0;
+    int transaction_size = 0;
+
+    if (sample_count < 0x201 || NULL != buff) {
+        //Each sample is 2 bytes. So, multiply by 2.
+        transaction_size = sample_count * 2;
+        if (buff == NULL) {
+            ret_val = E_ADXL363_NULL_PARAM;
+        } else {
+            read_fifo_req.rxData = adxl_363_fifo_rx_buff;
+            
+            // 1 byte is spent during transaction to send READ_FIFO command.
+            read_fifo_req.txLen = transaction_size + 1;
+            read_fifo_req.txData = adxl_363_fifo_tx_buff;
+            read_fifo_req.rxLen = read_fifo_req.txLen;
+            ret_val = MXC_SPI_MasterTransaction(&read_fifo_req);
+            if (ret_val == 0) {
+                memcpy(buff, adxl_363_fifo_rx_buff + 1, transaction_size);
+            }
+            memset(adxl_363_fifo_rx_buff, 0, ADXL_363_FIFO_RX_BUFF_SIZE);
+        }
+    } else {
+        ret_val = -3;
     }
-    else {
-      read_fifo_req.rxData = adxl_363_fifo_rx_buff;
-      read_fifo_req.txLen = __n + 1;
-      read_fifo_req.txData = adxl_363_fifo_tx_buff;
-      read_fifo_req.rxLen = read_fifo_req.txLen;
-      iVar1 = MXC_SPI_MasterTransaction(&read_fifo_req);
-      if (iVar1 == 0) {
-        memcpy(buff,adxl_363_fifo_rx_buff + 1,__n);
-      }
-      memset(adxl_363_fifo_rx_buff,0,0x401);
-    }
-  }
-  else {
-    iVar1 = -3;
-  }
-  return iVar1;
+    return ret_val;
 }
 
-int adxl363_fifo_set_sample_count(uint8_t sample_count)
+int adxl363_fifo_set_sample_count(uint16_t sample_count)
 {
-  int iVar1;
-  uint8_t fifo_ctrl_reg_val;
-  
-  fifo_ctrl_reg_val = '\0';
-  if (sample_count < 0x201) {
-    if ((0xff < sample_count) && (iVar1 = adxl363_reg_read('(',&fifo_ctrl_reg_val), iVar1 == 0)) {
-      adxl363_reg_write('(',fifo_ctrl_reg_val | 8);
+    int ret_val = 0;
+    uint8_t fifo_ctrl_reg_val = 0;
+
+    if (sample_count <= MAX_FIFO_SAMPLE_COUNT) {
+
+        if (0xff < sample_count) {
+            // We need to set AH bit to 1. However, we need to read
+            // FIFO control resister first not 
+            adxl363_reg_read(ADXL_363_REG_FIFO_CONTROL, &fifo_ctrl_reg_val);
+
+            adxl363_reg_write(ADXL_363_REG_FIFO_CONTROL, fifo_ctrl_reg_val | 8);
+        }
+        ret_val = adxl363_reg_write(ADXL_363_REG_FIFO_SAMPLES, sample_count);
+    } else {
+        ret_val = E_ADXL363_INVALID_PARAM;
     }
-    iVar1 = adxl363_reg_write(')',(uint8_t)sample_count);
-  }
-  else {
-    iVar1 = -3;
-  }
-  return iVar1;
+    return ret_val;
 }
 
-
-int adxl363_init(adxl_363_spi_t spi_conf,adxl_363_controller_t adxl_conf)
+int adxl363_init(adxl_363_spi_t spi_conf, adxl_363_controller_t adxl_conf)
 {
-  adxl_363_spi_t spi_conf_00;
-  int iVar1;
-  uint in_r1;
-  undefined4 in_r2;
-  undefined4 in_r3;
-  undefined4 unaff_r4;
-  mxc_spi_regs_t *spi;
-  undefined4 unaff_r5;
-  undefined6 in_stack_ffffffde;
-  adxl_range_t aStack_f;
-  
-  adxl363_spi_conf.spi_id = '\0';
-  adxl363_spi_conf.spi_ssid = '\0';
-  adxl363_spi_conf._2_2_ = 0;
-  adxl363_spi_conf.spi_speed = 0;
-  adxl363_spi_conf.spi_pins.clock = false;
-  adxl363_spi_conf.spi_pins.ss0 = false;
-  adxl363_spi_conf.spi_pins.ss1 = false;
-  adxl363_spi_conf.spi_pins.ss2 = false;
-  adxl363_spi_conf.spi_pins.miso = false;
-  adxl363_spi_conf.spi_pins.mosi = false;
-  adxl363_spi_conf.spi_pins.sdio2 = false;
-  adxl363_spi_conf.spi_pins.sdio3 = false;
-  adxl363_spi_conf._16_4_ = 0;
-  adxl363_conf.odr = ADXL_363_ODR_12_5;
-  adxl363_conf.range = ADXL_363_RANGE_2G;
-  if (in_r1 - 2400000 < 0x557301) {
-    spi_conf_00._6_6_ = in_stack_ffffffde;
-    spi_conf_00._0_6_ = spi_conf._0_6_;
-    spi_conf_00.spi_pins._4_4_ = unaff_r4;
-    spi_conf_00._16_4_ = unaff_r5;
-    iVar1 = __initialize_mxc_spi(spi_conf_00,adxl_conf);
-    if (iVar1 == 0) {
-      if (((uint8_t)adxl_conf & 0xff) == 0) {
-        spi = (mxc_spi_regs_t *)0x40046000;
-      }
-      else if (((uint8_t)adxl_conf & 0xff) == 1) {
-        spi = (mxc_spi_regs_t *)0x400be000;
-      }
-      else {
-        spi = (mxc_spi_regs_t *)0x0;
-      }
-      MXC_SPI_SetDataSize(spi,8);
-      read_reg_req.txData = read_reg_pack;
-      read_reg_req.ssDeassert = 1;
-      aStack_f = adxl_conf.range;
-      read_fifo_req.ssIdx = (int)aStack_f;
-      write_reg_req.txData = write_reg_pack;
-      write_reg_req.ssDeassert = 1;
-      read_fifo_req.ssDeassert = 1;
-      read_fifo_req.spi = spi;
-      read_reg_req.spi = spi;
-      read_reg_req.ssIdx = read_fifo_req.ssIdx;
-      write_reg_req.spi = spi;
-      write_reg_req.ssIdx = read_fifo_req.ssIdx;
-      memset(adxl_363_fifo_tx_buff,0,0x401);
-      memset(adxl_363_fifo_rx_buff,0,0x401);
-      adxl_363_fifo_tx_buff[0] = '\r';
-      iVar1 = adxl363_reg_write(',',(byte)spi_conf.spi_speed | spi_conf.spi_speed._1_1_);
-      adxl363_spi_conf._16_4_ = spi_conf._0_4_;
-      adxl363_conf = spi_conf.spi_speed;
-      adxl363_spi_conf._0_4_ = (uint)(uint8_t)adxl_conf;
-      adxl363_spi_conf.spi_speed = in_r1;
-      adxl363_spi_conf.spi_pins._0_4_ = in_r2;
-      adxl363_spi_conf.spi_pins._4_4_ = in_r3;
+    int ret_val = 0;
+    mxc_spi_regs_t *spi = NULL;
+    spi = MXC_SPI_GET_SPI(spi_conf.spi_id);
+
+    ret_val = __initialize_mxc_spi(spi_conf, adxl_conf);
+    if (ret_val == E_NO_ERROR) {
+        MXC_SPI_SetDataSize(spi, 8);
+
+        read_reg_req.txData = read_reg_pack;
+        read_reg_req.ssDeassert = 1;
+        read_fifo_req.ssIdx = spi_conf.spi_ssid;
+        write_reg_req.txData = write_reg_pack;
+        write_reg_req.ssDeassert = 1;
+        read_fifo_req.ssDeassert = 1;
+        read_fifo_req.spi = spi;
+        read_reg_req.spi = spi;
+        read_reg_req.ssIdx = read_fifo_req.ssIdx;
+        write_reg_req.spi = spi;
+        write_reg_req.ssIdx = read_fifo_req.ssIdx;
+
+        // Clear fifo TX transaction buffer.
+        memset(adxl_363_fifo_tx_buff, 0, ADXL_363_FIFO_TX_BUFF_SIZE);
+        adxl_363_fifo_tx_buff[0] = ADXL363_CMD_READ_FIFO;
+
+        // Clear fifo RX transaction buffer.
+        memset(adxl_363_fifo_rx_buff, 0, ADXL_363_FIFO_RX_BUFF_SIZE);
+
+        //
+        ret_val = adxl363_reg_write(ADXL_363_REG_FILTER_CTL, adxl_conf.odr | adxl_conf.range);
+        // adxl363_spi_conf._16_4_ = spi_conf._0_4_;
+        // adxl363_conf = spi_conf.spi_speed;
+        // adxl363_spi_conf._0_4_ = (uint)(uint8_t)adxl_conf;
+        // adxl363_spi_conf.spi_speed = in_r1;
+        // adxl363_spi_conf.spi_pins._0_4_ = in_r2;
+        // adxl363_spi_conf.spi_pins._4_4_ = in_r3;
     }
-  }
-  else {
-    iVar1 = -3;
-  }
-  return iVar1;
+ 
+    return ret_val;
 }
 
+CODE_TO_COMMENT_OUT(
+    int adxl363_intmap_conf(uint8_t int_mask, uint8_t intpin_id)
+    {
+        adxl363_reg_t reg_addr;
+        int ret_val = 0;
 
-void adxl363_intmap1_cb(void *cb_param)
+        if (intpin_id == 1 || intpin_id == 2) {
+            if (intpin_id == 1) {
+                reg_addr = ADXL_363_REG_INTMAP1;
+            } else {
+                reg_addr = ADXL_363_REG_INTMAP2;
+            } 
 
+            ret_val = adxl363_reg_write(reg_addr, int_mask);
+        } else {
+            ret_val = E_ADXL363_INVALID_PARAM;
+        }
+        return ret_val;
+    }
+)
+
+int adxl363_reg_read(adxl363_reg_t reg_addr, uint8_t *read_buff)
 {
-  map1_int = '\0';
-  return;
+    int ret_val = 0;
+    uint8_t read_data[3] = {0};
+
+    if (read_buff != NULL) {
+        ret_val = __adxl363_reg_read(reg_addr, read_data, 1);
+        *read_buff = read_data[2];
+    }else{
+      ret_val = E_ADXL363_NULL_PARAM;
+    }
+
+    return ret_val;
 }
 
-int adxl363_intmap_conf(uint8_t int_mask,uint8_t intpin_id)
-
+int adxl363_reg_write(adxl363_reg_t reg_addr, uint8_t val)
 {
-  adxl363_reg_t reg_addr;
-  int iVar1;
-  
-  if ((byte)(intpin_id - 1) < 2) {
-    if (intpin_id == '\x01') {
-      reg_addr = '*';
-    }
-    else if (intpin_id == '\x02') {
-      reg_addr = '+';
-    }
-    else {
-      reg_addr = 0xff;
-    }
-    iVar1 = adxl363_reg_write(reg_addr,int_mask);
-  }
-  else {
-    iVar1 = -3;
-  }
-  return iVar1;
-}
+    int ret_val = 0;
 
-int adxl363_reg_read(adxl363_reg_t reg_addr,uint8_t *read_buff)
-
-{
-  int iVar1;
-  uint8_t read_data [3];
-  
-  if (read_buff != (uint8_t *)0x0) {
-    read_data[0] = '\0';
-    read_data[1] = '\0';
-    read_data[2] = '\0';
-    iVar1 = __adxl363_reg_read(reg_addr,read_data,1);
-    *read_buff = read_data[2];
-    return iVar1;
-  }
-  return -3;
-}
-
-int adxl363_reg_write(adxl363_reg_t reg_addr,uint8_t val)
-
-{
-  int iVar1;
-  
-  write_reg_req.txData = write_reg_pack;
-  write_reg_req.txLen = 3;
-  write_reg_pack[1] = reg_addr;
-  write_reg_pack[2] = val;
-  iVar1 = MXC_SPI_MasterTransaction(&write_reg_req);
-  return iVar1;
+    write_reg_req.txData = write_reg_pack;
+    write_reg_req.txLen = 3;
+    write_reg_pack[1] = reg_addr;
+    write_reg_pack[2] = val;
+    ret_val = MXC_SPI_MasterTransaction(&write_reg_req);
+    return ret_val;
 }
 
 int adxl363_shutdown(void)
-
 {
-  int iVar1;
-  
-  adxl363_reg_write('\x1f','R');
-  iVar1 = MXC_SPI_Shutdown(write_reg_req.spi);
-  __reset_variables();
-  return iVar1;
+    int ret_val = 0;
+    adxl363_reg_write(ADXL_363_REG_RESET, ADXL_363_REG_RESET_CODE);
+    ret_val = MXC_SPI_Shutdown(write_reg_req.spi);
+    __reset_variables();
+    return ret_val;
 }
-
--- Libraries/MiscDrivers/ACCEL/ADXL/adxl363.h
+++ Libraries/MiscDrivers/ACCEL/ADXL/adxl363.h
@@ -1,9 +1,174 @@
+#ifndef _ADXL363_H
+#define _ADXL363_H
+
 #include "stdint.h"
 #include "stdlib.h"
 #include "stdio.h"
+#include "string.h"
 #include "spi.h"
 
-typedef enum adxl_odr_t {
+typedef enum{
+    E_ADXL363_NULL_PARAM,
+    E_ADXL363_INVALID_PARAM,
+    E_ADXL363_NO_ERR = 0
+}E_ADXL363_ERR;
+
+/**
+ * @brief Command identifiers
+*/
+#define ADXL363_CMD_READ 0x0A
+#define ADXL363_CMD_WRITE 0x0B
+#define ADXL363_CMD_READ_FIFO 0x0D
+
+// Register definitions
+#define ADXL_363_REG_DEVID_AD             (0x00)
+#define ADXL_363_REG_DEVID_MST            (0x01)
+#define ADXL_363_REG_DEVID                (0x02)
+#define ADXL_363_REG_REVID                (0x03)
+#define ADXL_363_REG_XDATA                (0x08)
+#define ADXL_363_REG_YDATA                (0x09)
+#define ADXL_363_REG_ZDATA                (0x0a)
+#define ADXL_363_REG_STATUS               (0x0b)
+#define ADXL_363_REG_FIFO_ENTRIES_L       (0x0c)
+#define ADXL_363_REG_FIFO_ENTRIES_H       (0x0d)
+#define ADXL_363_REG_XDATA_L              (0x0e)
+#define ADXL_363_REG_XDATA_H              (0x0f)
+#define ADXL_363_REG_YDATA_L              (0x10)
+#define ADXL_363_REG_YDATA_H              (0x11)
+#define ADXL_363_REG_ZDATA_L              (0x12)
+#define ADXL_363_REG_ZDATA_H              (0x13)
+#define ADXL_363_REG_TEMP_L               (0x14)
+#define ADXL_363_REG_TEMP_H               (0x15)
+#define ADXL_363_REG_ADC_DATA_L           (0x16)
+#define ADXL_363_REG_ADC_DATA_H           (0x17)
+#define ADXL_363_REG_SOFT_RESET           (0x1f)
+#define ADXL_363_REG_THRESH_ACT_L         (0x20)
+#define ADXL_363_REG_THRESH_ACT_H         (0x21)
+#define ADXL_363_REG_TIME_ACT             (0x22)
+#define ADXL_363_REG_THRESH_INACT_L       (0x23)
+#define ADXL_363_REG_THRESH_INACT_H       (0x24)
+#define ADXL_363_REG_TIME_INACT_L         (0x25)
+#define ADXL_363_REG_TIME_INACT_H         (0x26)
+#define ADXL_363_REG_ACT_INACT_CTL        (0x27)
+#define ADXL_363_REG_FIFO_CONTROL         (0x28)
+#define ADXL_363_REG_FIFO_SAMPLES         (0x29)
+#define ADXL_363_REG_INTMAP1              (0x2a)
+#define ADXL_363_REG_INTMAP2              (0x2b)
+#define ADXL_363_REG_FILTER_CTL           (0x2c)
+#define ADXL_363_REG_POWER_CTL            (0x2d)
+#define ADXL_363_REG_SELF_TEST            (0x2e)
+
+// Defining a fake register for reading from the FIFO
+#define ADXL_363_PSEUDOREG_FIFO           (0x30)
+
+// 0x22 TIME_ACT
+// Activity Time is measured in units of inverse ODR
+// (i.e. 1 unit = 2.5ms for ODR=400Hz)
+#define ADXL_363_REG_TIME_ACT_DEFAULT           (0x01)
+
+// 0x27 ACT_INACT_CTL
+#define ADXL_363_REG_ACT_INACT_CTL_ACT_EN       (0x01)
+#define ADXL_363_REG_ACT_INACT_CTL_ACT_REF      (0x02)
+#define ADXL_363_REG_ACT_INACT_CTL_INACT_EN     (0x04)
+#define ADXL_363_REG_ACT_INACT_CTL_INACT_REF    (0x08)
+#define ADXL_363_REG_ACT_INACT_CTL_LINKLOOP_MASK    (0x30)
+#define ADXL_363_REG_ACT_INACT_CTL_LINKLOOP_DEFAULT (0x00)
+#define ADXL_363_REG_ACT_INACT_CTL_LINKLOOP_LINKED  (0x10)
+#define ADXL_363_REG_ACT_INACT_CTL_LINKLOOP_LOOP    (0x30)
+#define ADXL_363_REG_ACT_INACT_CTL_DEFAULT      (0x00)
+
+// 0x28 FIFO_CONTROL
+#define ADXL_363_REG_FIFO_CTL_MODE_MASK         (0x03)
+#define ADXL_363_REG_FIFO_CTL_MODE_DIS          (0x00)
+#define ADXL_363_REG_FIFO_CTL_MODE_OLDEST       (0x01)
+#define ADXL_363_REG_FIFO_CTL_MODE_STREAM       (0x02)
+#define ADXL_363_REG_FIFO_CTL_MODE_TRIG         (0x03)
+
+// 0x2A INTMAP1
+#define ADXL_363_REG_INTMAP1_DATA_RDY           (0x01)
+#define ADXL_363_REG_INTMAP1_FIFO_RDY           (0x02)
+#define ADXL_363_REG_INTMAP1_FIFO_WATERMARK     (0x04)
+#define ADXL_363_REG_INTMAP1_FIFO_OVERRUN       (0x08)
+#define ADXL_363_REG_INTMAP1_ACT                (0x10)
+#define ADXL_363_REG_INTMAP1_INACT              (0x20)
+#define ADXL_363_REG_INTMAP1_AWAKE              (0x40)
+#define ADXL_363_REG_INTMAP1_ACTIVE_LOW         (0x80)
+
+// 0x2B INTMAP2
+#define ADXL_363_REG_INTMAP2_DATA_RDY           (0x01)
+#define ADXL_363_REG_INTMAP2_FIFO_RDY           (0x02)
+#define ADXL_363_REG_INTMAP2_FIFO_WATERMARK     (0x04)
+#define ADXL_363_REG_INTMAP2_FIFO_OVERRUN       (0x08)
+#define ADXL_363_REG_INTMAP2_ACT                (0x10)
+#define ADXL_363_REG_INTMAP2_INACT              (0x20)
+#define ADXL_363_REG_INTMAP2_AWAKE              (0x40)
+#define ADXL_363_REG_INTMAP2_ACTIVE_LOW         (0x80)
+
+// 0x2C FILTER_CTL
+#define ADXL_363_REG_FILTER_CTL_ODR_MASK        (0x07)
+#define ADXL_363_REG_FILTER_CTL_ODR_12HZ        (0x00)
+#define ADXL_363_REG_FILTER_CTL_ODR_25HZ        (0x01)
+#define ADXL_363_REG_FILTER_CTL_ODR_50HZ        (0x02)
+#define ADXL_363_REG_FILTER_CTL_ODR_100HZ       (0x03)
+#define ADXL_363_REG_FILTER_CTL_ODR_200HZ       (0x04)
+#define ADXL_363_REG_FILTER_CTL_ODR_400HZ       (0x05)
+#define ADXL_363_REG_FILTER_CTL_EXT_SAMPLE_EN   (0x08)
+#define ADXL_363_REG_FILTER_CTL_EXT_SAMPLE_DIS  (0x00)
+#define ADXL_363_REG_FILTER_CTL_HALF_BW_EN      (0x10)
+#define ADXL_363_REG_FILTER_CTL_HALF_BW_DIS     (0x00)
+#define ADXL_363_REG_FILTER_CTL_RANGE_MASK      (0xC0)
+#define ADXL_363_REG_FILTER_CTL_RANGE_2G        (0x00)
+#define ADXL_363_REG_FILTER_CTL_RANGE_4G        (0x40)
+#define ADXL_363_REG_FILTER_CTL_RANGE_8G        (0x80)
+#define ADXL_363_REG_FILTER_CTL_DEFAULT   (ADXL_363_REG_FILTER_CTL_ODR_400HZ | ADXL_363_REG_FILTER_CTL_EXT_SAMPLE_EN | \
+                                       ADXL_363_REG_FILTER_CTL_HALF_BW_DIS | ADXL_363_REG_FILTER_CTL_RANGE_8G)
+
+// 0x2D POWER_CTL
+#define ADXL_363_REG_POWER_CTL_MODE_MASK        (0x03)
+#define ADXL_363_REG_POWER_CTL_MODE_STBY        (0x00)
+#define ADXL_363_REG_POWER_CTL_MODE_MEAS        (0x02)
+#define ADXL_363_REG_POWER_CTL_AUTOSLEEP        (0x04)
+#define ADXL_363_REG_POWER_CTL_WAKEUP           (0x08)
+#define ADXL_363_REG_POWER_CTL_LOW_NOISE_MASK   (0x30)
+#define ADXL_363_REG_POWER_CTL_LOW_NOISE_NORMAL (0x00)
+#define ADXL_363_REG_POWER_CTL_LOW_NOISE_LOW    (0x10)
+#define ADXL_363_REG_POWER_CTL_LOW_NOISE_ULOW   (0x20)
+#define ADXL_363_REG_POWER_CTL_EXT_CLK_EN       (0x40)
+#define ADXL_363_REG_POWER_CTL_EXT_CLK_DIS      (0x00)
+#define ADXL_363_REG_POWER_CTL_EXT_ADC_EN       (0x80)
+#define ADXL_363_REG_POWER_CTL_EXT_ADC_DIS      (0x00)
+#define ADXL_363_REG_POWER_CTL_DEFAULT    (ADXL_363_REG_POWER_CTL_MODE_MEAS | ADXL_363_REG_POWER_CTL_LOW_NOISE_NORMAL | \
+                                       ADXL_363_REG_POWER_CTL_EXT_CLK_DIS | ADXL_363_REG_POWER_CTL_EXT_ADC_EN)
+
+// PATCH LAYER
+// These defines are to make the ADXL363 look like the ADXL372 as much as possible
+#define ADXL_363_REG_PARTID               ADXL_363_REG_DEVID
+#define ADXL_363_REG_FIFO_ENTRIES2        ADXL_363_REG_FIFO_ENTRIES_H
+#define ADXL_363_REG_FIFO_ENTRIES         ADXL_363_REG_FIFO_ENTRIES_L
+#define ADXL_363_REG_FIFO_ENTRIES_PATCH   ADXL_363_REG_FIFO_ENTRIES_L
+#define ADXL_363_REG_FIFO_CTL             ADXL_363_REG_FIFO_CONTROL
+#define ADXL_363_REG_RESET                ADXL_363_REG_SOFT_RESET
+#define ADXL_363_REG_FIFO_DATA            ADXL_363_PSEUDOREG_FIFO
+#define ADXL_363_REG_THRESH_ACT_X_PATCH   ADXL_363_REG_THRESH_ACT_L
+#define ADXL_363_REG_THRESH_ACT_Y_PATCH   ADXL_363_REG_THRESH_ACT_L
+#define ADXL_363_REG_THRESH_ACT_Z_PATCH   ADXL_363_REG_THRESH_ACT_L
+#define ADXL_363_REG_TIME_INACT_PATCH     ADXL_363_REG_TIME_INACT_L
+#define ADXL_363_REG_THRESH_INACT_X_PATCH ADXL_363_REG_THRESH_INACT_L
+#define ADXL_363_REG_THRESH_INACT_Y_PATCH ADXL_363_REG_THRESH_INACT_L
+#define ADXL_363_REG_THRESH_INACT_Z_PATCH ADXL_363_REG_THRESH_INACT_L
+#define ADXL_363_REG_TIME_INACT_PATCH     ADXL_363_REG_TIME_INACT_L
+
+// Different devices order the bytes H/L in different directions.
+#define ADXL_363_REG_XDATA_PATCH          ADXL_363_REG_XDATA_L
+#define ADXL_363_REG_YDATA_PATCH          ADXL_363_REG_YDATA_L
+#define ADXL_363_REG_ZDATA_PATCH          ADXL_363_REG_ZDATA_L
+
+// 0x1F SOFT RESET
+#define ADXL_363_REG_RESET_CODE (0x52)
+
+typedef uint8_t adxl363_reg_t;
+
+typedef enum adxl_odr_t{
     ADXL_363_ODR_12_5=0,
     ADXL_363_ODR_25=1,
     ADXL_363_ODR_50=2,
@@ -12,23 +177,23 @@ typedef enum adxl_odr_t {
     ADXL_363_ODR_400=5
 } adxl_odr_t;
 
-typedef enum adxl_range_t {
+typedef enum {
     ADXL_363_RANGE_2G=0,
     ADXL_363_RANGE_4G=64,
     ADXL_363_RANGE_8G=128
 } adxl_range_t;
 
-typedef struct  {
-    enum adxl_odr_t odr;
-    enum adxl_range_t range;
-};
+typedef struct adxl_363_controller_t{
+    adxl_odr_t odr;
+    adxl_range_t range;
+}adxl_363_controller_t;
 
-struct adxl_363_spi_t {
+typedef struct adxl_363_spi_t{
     uint8_t spi_id;
     uint8_t spi_ssid;
     uint32_t spi_speed;
     mxc_spi_pins_t spi_pins;
-};
+}adxl_363_spi_t;
 
 typedef enum adxl_axis_t {
     X_AXIS=88,
@@ -36,14 +201,28 @@ typedef enum adxl_axis_t {
     Z_AXIS=90
 } adxl_axis_t;
 
-int adxl363_axis_read(short * axis_data, adxl_axis_t axis_id);
+#define COMMENT_OUT 0
+#if COMMENT_OUT
+    #define CODE_TO_COMMENT_OUT(code)
+#else
+    #define CODE_TO_COMMENT_OUT(code) code
+#endif
+
+/** API Declarations */
+int adxl363_axis_read(int16_t * axis_data, adxl_axis_t axis_id);
 int adxl363_enable_measurement(uint8_t enable);
 int adxl363_fifo_enable_mode(uint8_t mode);
-int adxl363_fifo_get_sample_count(uint16_t * sample_count);
-int adxl363_fifo_read(uint16_t * buff, uint16_t sample_count);
+int adxl363_fifo_get_sample_count(uint16_t *sample_count);
+int adxl363_fifo_read(uint8_t *buff, uint16_t sample_count);
 int adxl363_fifo_set_sample_count(uint16_t sample_count);
+
 int adxl363_init(adxl_363_spi_t spi_conf, adxl_363_controller_t adxl_conf);
-int adxl363_intmap_conf(uint8_t int_mask, uint8_t intpin_id);
+
+CODE_TO_COMMENT_OUT(int adxl363_intmap_conf(uint8_t int_mask, uint8_t intpin_id);)
+
 int adxl363_reg_read(adxl363_reg_t reg_addr, uint8_t * read_buff);
 int adxl363_reg_write(adxl363_reg_t reg_addr, uint8_t val);
 int adxl363_shutdown(void);
+
+
+#endif
