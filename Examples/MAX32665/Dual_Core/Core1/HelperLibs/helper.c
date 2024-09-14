ce03e0ca136d50d684d2b18c4b2662110ebefe33
ANALOG\OKilic <Omercan.Kilic@analog.com>
2024-09-14 13:48:24 +0300

    Packs are delivered from core1 to core0 succesfully. ODR is 100 now

--- Examples/MAX32665/Dual_Core/Core1/HelperLibs/helper.c
+++ Examples/MAX32665/Dual_Core/Core1/HelperLibs/helper.c
@@ -12,3 +12,15 @@ void send_pack_to_host(adxl363_sample_pkg_t *sample_pack)
     printf("%u.%u %hd %hd %hd\r\n", sample_pack->sec, sample_pack->subsec, sample_pack->x_val,
            sample_pack->y_val, sample_pack->z_val);
 }
+
+
+void print_complete_pack(uint8_t *pack){
+    int sizeof_pack = pack[0];
+    int counter = 0;
+    uint8_t *single_pack = &pack[1];
+                
+    while(counter < sizeof_pack){
+        send_pack_to_host((adxl363_sample_pkg_t*)&pack[counter*(1+(sizeof(adxl363_sample_pkg_t)))+1]);
+        counter +=1;
+    }
+}
--- Examples/MAX32665/Dual_Core/shared_data.c
+++ Examples/MAX32665/Dual_Core/shared_data.c
@@ -2,6 +2,5 @@
 #include "adxl363.h"
 
 uint8_t sensor_pack_buffer[SENSOR_PACK_BUFF_LENGTH];
-struct sensor_pack_t sensor_pack = {.pack_list = sensor_pack_buffer, .length=0, .sem_id = SENSOR_PACK_SEM_ID};
 uint8_t ready_flag = 0;
 uint8_t sem_ready_flag = PACK_READY_SEM_ID;
