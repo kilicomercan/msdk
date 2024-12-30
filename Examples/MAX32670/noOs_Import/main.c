ecbe7bf748079017add17b16de0acba943f8ace8
ANALOG\OKilic <Omercan.Kilic@analog.com>
2024-12-30 15:51:15 +0300

    Add no-OS drivers importing example to MSDK.

---
+++ Examples/MAX32670/noOs_Import/main.c
@@ -0,0 +1,49 @@
+/******************************************************************************
+ *
+ * Copyright (C) 2022-2023 Maxim Integrated Products, Inc. (now owned by 
+ * Analog Devices, Inc.),
+ * Copyright (C) 2023-2024 Analog Devices, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+/**
+ * @file    main.c
+ * @brief   noOs_Import Example!
+ * @details This example configures the environment for no-OS driver usage in MSDK.
+ */
+
+/***** Includes *****/
+#include <stdio.h>
+#include <stdint.h>
+#include "mxc_device.h"
+#include "led.h"
+#include "board.h"
+#include "mxc_delay.h"
+#include "ad74416h.h"
+
+// *****************************************************************************
+int main(void)
+{
+    /**
+     * @note Write your test code here for ad74416h. You should not encounter 
+     * with a compilation error.However, ad74416h driver may have errors in
+     * itself. I can not guarantee that the driver will work correctly.
+     * You should modify the driver according to your needs. See the driver 
+     * files in msdk\Libraries\MiscDrivers\no_os\adc-dac\ad74416h directory.
+     * Do not forget to check the build log for errors.
+     */
+    
+    return 0;
+}
