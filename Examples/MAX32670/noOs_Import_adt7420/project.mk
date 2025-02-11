a749d98dbb1356b1ff4b3bea9042f960f59ac827
ANALOG\OKilic <Omercan.Kilic@analog.com>
2025-02-11 13:42:26 +0300

    Add adt7420 example

---
+++ Examples/MAX32670/noOs_Import_adt7420/project.mk
@@ -0,0 +1,68 @@
+###############################################################################
+ #
+ # Copyright (C) 2024 Analog Devices, Inc.
+ #
+ # Licensed under the Apache License, Version 2.0 (the "License");
+ # you may not use this file except in compliance with the License.
+ # You may obtain a copy of the License at
+ #
+ #     http://www.apache.org/licenses/LICENSE-2.0
+ #
+ # Unless required by applicable law or agreed to in writing, software
+ # distributed under the License is distributed on an "AS IS" BASIS,
+ # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ # See the License for the specific language governing permissions and
+ # limitations under the License.
+ #
+ ##############################################################################
+# This file can be used to set build configuration
+# variables.  These variables are defined in a file called 
+# "Makefile" that is located next to this one.
+
+# For instructions on how to use this system, see
+# https://analogdevicesinc.github.io/msdk/USERGUIDE/#build-system
+
+#BOARD=FTHR_RevA
+# ^ For example, you can uncomment this line to make the 
+# project build for the "FTHR_RevA" board.
+
+# **********************************************************
+
+# Add your config here!
+
+dev_type = temperature
+dev_name = adt7420
+noOs_src_api_dir = $(MAXIM_PATH)/Libraries/noOS
+
+
+# SRCS += $(noOs_src_api_dir)/no_os_alloc.c
+# SRCS += $(noOs_src_api_dir)/api/no_os_spi.c
+# SRCS += $(noOs_src_api_dir)/api/no_os_gpio.c
+# SRCS += $(noOs_src_api_dir)/api/no_os_i2c.c
+
+# SRCS += $(noOs_src_api_dir)/no_os_util.c 
+# SRCS += $(noOs_src_api_dir)/no_os_mutex.c
+# SRCS += $(noOs_src_api_dir)/no_os_error.c
+
+# Add header libraries
+IPATH += $(MAXIM_PATH)/Libraries/noOS/include
+IPATH += $(MAXIM_PATH)/Libraries/noOS/maxim/$(TARGET)
+IPATH += $(noOs_src_api_dir)/iio
+IPATH += $(noOs_src_api_dir)/iio/iio_app
+
+
+SRCS += $(wildcard $(noOs_src_api_dir)/src/*.c)
+SRCS += $(wildcard $(noOs_src_api_dir)/src/api/*.c)
+SRCS += $(wildcard $(noOs_src_api_dir)/iio/*.c)
+SRCS += $(wildcard $(noOs_src_api_dir)/iio/iio_app/*.c)
+
+# We will use spi interface to control the accelerometer.
+# So, we are adding maxim_api.c file to the project for compilation.
+SRCS += $(MAXIM_PATH)/Libraries/noOS/maxim/$(TARGET)/maxim_delay.c
+SRCS += $(MAXIM_PATH)/Libraries/noOS/maxim/$(TARGET)/maxim_spi.c
+SRCS += $(MAXIM_PATH)/Libraries/noOS/maxim/$(TARGET)/maxim_gpio.c
+SRCS += $(MAXIM_PATH)/Libraries/noOS/maxim/$(TARGET)/maxim_i2c.c
+
+IPATH += $(MAXIM_PATH)/Libraries/MiscDrivers/no_os/$(dev_type)/$(dev_name)
+VPATH += $(MAXIM_PATH)/Libraries/MiscDrivers/no_os/$(dev_type)/$(dev_name)
+
